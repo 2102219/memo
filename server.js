740229efaae1c2e55013802947e1e3a1251126a1
yuya2102219 <yuya25142203@gmail.com>
2025-01-30 05:41:30 +0900

    提出用ver1.4

--- server.js
+++ server.js
@@ -1,35 +1,17 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-import { extname } from "https://deno.land/std/path/mod.ts";
+import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 
-const fileDirectory = "./memo"; 
+Deno.serve(async (req) => {
+  const pathname = new URL(req.url).pathname;
+  console.log(pathname);
 
-serve(async (req) => {
-  const url = new URL(req.url);
-  let path = url.pathname;
-
-  if (path === "/memo") {
-    path = "/index.html"; // ルートをindex.htmlにリダイレクト
+  if (req.method === "GET" && pathname === "/welcome-message") {
+    return new Response("jigインターンへようこそ！");
   }
 
-  const filePath = `${fileDirectory}${path}`;
-
-  try {
-    const content = await Deno.readFile(filePath);
-    const fileExtension = extname(filePath).toLowerCase();
-
-    let contentType = "text/plain";
-    if (fileExtension === ".html") {
-      contentType = "text/html";
-    } else if (fileExtension === ".css") {
-      contentType = "text/css";
-    } else if (fileExtension === ".js") {
-      contentType = "application/javascript";
-    }
-
-    return new Response(content, { headers: { "content-type": contentType } });
-  } catch {
-    return new Response("404: Not Found", { status: 404 });
-  }
+  return serveDir(req, {
+    fsRoot: "memo",
+    urlRoot: "",
+    showDirListing: true,
+    enableCors: true,
+  });
 });
-
-console.log("Server is running on http://localhost:8000");
